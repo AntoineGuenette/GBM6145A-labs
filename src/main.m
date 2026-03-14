aee7e75582a6795cdc1d10b48203333a1ef10be8
AntoineGuenette <anto.guenette@gmail.com>
2026-03-14 14:31:02 -0400

    Update main script

--- src/main.m
+++ src/main.m
@@ -1,12 +1,24 @@
 % Load subject info
-subjects = subject_info();
-fprintf("Loaded information of subject %s\n", subjects(1).code)
-fprintf("Loaded information of subject %s\n", subjects(2).code)
-fprintf("Loaded information of subject %s\n", subjects(3).code)
-fprintf("Loaded information of subject %s\n", subjects(4).code)
+old_subjects = get_old_subject_info();
+subjects = get_subject_info();
 
 % Convert .emt files to .csv files
-% emtFolder2csv('../data_old/5_ROSA')
+emtFolder2csv('../data_old/1_SCHP')
+emtFolder2csv('../data_old/3_MICA')
+emtFolder2csv('../data_old/4_STAV')
+emtFolder2csv('../data_old/5_ROSA')
 
+% Load one EMG file
 file_path = '../data_old/5_ROSA/0004~ac~Trial3_WithOrth.csv';
-plot_tracks(file_path, subjects(4).bmi);
+data = readtable(file_path);
+
+% Extract time and raw
+time = data.Time;
+EMG_raw = data.EMG_1;
+
+% Process EMG and show each step
+EMG_processed = process_EMG(EMG_raw, time, 1000, ...
+    subjects(4).bmi, ShowGraph=true);
+
+% Show all tracks
+plot_tracks(file_path, subjects(4).bmi)
