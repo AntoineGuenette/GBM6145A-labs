7686a77c283c5f6ea20cf5dc63b69b1b5d21f67d
AntoineGuenette <anto.guenette@gmail.com>
2026-03-17 14:23:54 -0400

    Add first version of data analysis on subject 1

--- src/main.m
+++ src/main.m
@@ -8,17 +8,36 @@ emtFolder2csv('../data_old/3_MICA')
 emtFolder2csv('../data_old/4_STAV')
 emtFolder2csv('../data_old/5_ROSA')
 
-% Load one EMG file
-file_path = '../data_old/5_ROSA/0004~ac~Trial3_WithOrth.csv';
-data = readtable(file_path);
+emtFolder2csv('../data/GUEA_ses1')
 
-% Extract time and raw
-time = data.Time;
-EMG_raw = data.EMG_1;
+% ------- SUBJECT 1 --------
 
-% Process EMG and show each step
-EMG_processed = process_EMG(EMG_raw, time, 1000, ...
-    subjects(4).bmi, ShowGraph=true);
+save_dir = "../figs/GUEA_ses1/all_tracks";
+titles = get_titles_GUEA_ses1();
 
-% Show all tracks
-plot_tracks(file_path, subjects(4).bmi)
+% Process each file in the folder
+files = dir(fullfile('../data/GUEA_ses1', '*.csv'));
+for k = 1:numel(files)
+
+    % Construct full input path
+    emg_Path = fullfile(files(k).folder, files(k).name);
+
+    % Get file name
+    file_name = string(files(k).name);
+    save_path = fullfile(save_dir, titles(file_name));
+
+    % Get test name and deduce title
+    test_name = titles(file_name);
+    title = strrep(test_name, '_', ' ');
+    title = strrep(title, '-', ' - ');
+    
+    % Show all tracks
+    if file_name == "0038~aa~t31.csv"
+        plot_tracks(emg_Path, save_path, title, subjects(1).bmi, ...
+        BicepTrack=1, TricepTrack=2, AntDeltTrack=5, PostDeltTrack=4)
+    else
+        plot_tracks(emg_Path, save_path, title, subjects(1).bmi)
+    end
+
+end
+
