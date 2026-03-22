7e16da1d80b34a2a0465131f22f33f08c5b30119
AntoineGuenette <anto.guenette@gmail.com>
2026-03-22 12:09:33 -0400

    Update the CAI execution

--- src/main.m
+++ src/main.m
@@ -25,242 +25,241 @@ movements("Elbow_Flexion") = struct('agoniste', "Bicep", 'antagoniste', "Tricep"
 movements("Pointing_Task") = struct('agoniste', ["Tricep","DeltAnt"], 'antagoniste', ["Bicep","DeltPost"]);
 movements("HFT_Large_Light_Objects") = struct('agoniste', ["Bicep","DeltAnt"], 'antagoniste', ["Tricep","DeltPost"]);
 movements("Switch_Joystick_and_Grid_Game") = struct('agoniste', ["Bicep","DeltAnt"], 'antagoniste', ["Tricep","DeltPost"]);
-movements("JAMAR_Palmar_Grip") = struct('agoniste', "Bicep", 'antagoniste', "Tricep");
-movements("HFT_Feeding_task") = struct('agoniste', "Bicep", 'antagoniste', "Tricep");
+movements("JAMAR_Palmar_Grip") = struct('agoniste', ["Bicep","DeltAnt"], 'antagoniste', ["Tricep","DeltPost"]);
+movements("HFT_Feeding_task") = struct('agoniste', ["Bicep","DeltAnt"], 'antagoniste', ["Tricep","DeltPost"]);
 movements("Box_and_Blocs_Test") = struct('agoniste', ["Tricep","DeltAnt"], 'antagoniste', ["Bicep","DeltPost"]);
 
-% % ------- SUBJECT 1 / SESSION 1 --------
-% 
-% data_dir = "../data/GUEA_ses1";
-% files = dir(fullfile(data_dir, '*.csv'));
-% all_tracks_dir = "../figs/GUEA_ses1/all_tracks";
-% superimpose_dir = "../figs/GUEA_ses1/superimpose";
-% CAI_dir = "../figs/GUEA_ses1/CAI";
-% 
-% % -- Build all tracks plot --
-% fprintf("\nCreating all tracks plots for GUEA_ses1...\n")
-% for k = 1:numel(files)
-% 
-%     % Construct full input path
-%     emg_Path = fullfile(files(k).folder, files(k).name);
-% 
-%     % Get file name
-%     file_name = string(files(k).name);
-% 
-%     % Get title
-%     title = strrep(file_name, '.csv', '');
-%     save_path = fullfile(all_tracks_dir, title);
-%     title = strrep(title, '_', ' ');
-%     title = strrep(title, '-', ' - ');
-% 
-%     % Show all tracks
-%     if file_name == "Serie_3-Group_3-Task_31-JAMAR_Palmar_Grip-Device_(DynAReach).csv"
-%         plot_tracks(emg_Path, save_path, title, subjects(1).bmi, ...
-%         BicepTrack=1, TricepTrack=2, AntDeltTrack=5, PostDeltTrack=4)
-%     else
-%         plot_tracks(emg_Path, save_path, title, subjects(1).bmi)
-%     end
-% 
-% end
-% fprintf("All tracks plots successfully created for GUEA_ses1.\n")
-% 
-% % -- Build superimpose plots --
-% fprintf("\nCreating superimpose plots for GUEA_ses1...\n")
-% plot_all_superimpose(data_dir, superimpose_dir)
-% fprintf("All superimpose plots successfully created for GUEA_ses1.\n")
-% 
-% % -- Build CAI plots --
-% fprintf("\nComputing CAI for GUEA_ses1...\n")
-% 
-% % Store last Rest file per group
-% last_rest_file = "";
-% 
-% for k = 1:numel(files)
-% 
-%     file_name = string(files(k).name);
-%     file_path = fullfile(data_dir, file_name);
-% 
-%     % Parse filename
-%     info = parse_filename(file_path);
-% 
-%     % Skip invalid files
-%     if isnan(info.group) || info.task_name == ""
-%         continue;
-%     end
-% 
-%     % Identify Rest file (per group!)
-%     if contains(info.task_name, "Rest")
-%         last_rest_file = file_path;
-%         fprintf("Rest file detected: %s\n", file_name);
-%         continue;
-%     end
-% 
-%     % Find matching movement key
-%     keys_list = movements.keys();
-%     matchedKey = "";
-% 
-%     for i = 1:length(keys_list)
-%         if startsWith(info.task_name, keys_list(i))
-%             matchedKey = keys_list(i);
-%             break;
-%         end
-%     end
-% 
-%     % If match found → compute CAI
-%     if matchedKey ~= ""
-% 
-%         % Check if Rest exists
-%         if last_rest_file == ""
-%             warning("No Rest file available yet. Skipping %s", file_name);
-%             continue;
-%         end
-% 
-%         file_path_rest = last_rest_file;
-% 
-%         Ag = movements(matchedKey).agoniste;
-%         Antag = movements(matchedKey).antagoniste;
-% 
-%         for j = 1:length(Ag)
-%             find_CAI( ...
-%                 last_rest_file, ...
-%                 file_path, ...
-%                 subjects(1).bmi, ...
-%                 "Ag", Ag(j), ...
-%                 "Antag", Antag(j), ...
-%                 "title", info.task_name, ...
-%                 "task", string(info.task), ...
-%                 "save_folder", CAI_dir, ...
-%                 "save_csv", CAI_results_path, ...
-%                 "subject","GUEA_ses1", ...
-%                 "device", info.device ...
-%             );
-%         end
-% 
-%     else
-%         warning("Task '%s' not recognized in dictionary.", info.task_name);
-%     end
-% 
-% end
-% fprintf("CAI computation completed for GUEA_ses1.\n")
-% 
-% 
-% % ------- SUBJECT 2 / SESSION 1 --------
-% 
-% data_dir = "../data/RABA_ses1";
-% files = dir(fullfile(data_dir, '*.csv'));
-% all_tracks_dir = "../figs/RABA_ses1/all_tracks";
-% superimpose_dir = "../figs/RABA_ses1/superimpose";
-% CAI_dir = "../figs/RABA_ses1/CAI";
-% 
-% % -- Build all tracks plot --
-% fprintf("\nCreating all tracks plots for RABA_ses1...\n")
-% for k = 1:numel(files)
-% 
-%     % Construct full input path
-%     emg_Path = fullfile(files(k).folder, files(k).name);
-% 
-%     % Get file name
-%     file_name = string(files(k).name);
-% 
-%     % Get title
-%     title = strrep(file_name, '.csv', '');
-%     save_path = fullfile(all_tracks_dir, title);
-%     title = strrep(title, '_', ' ');
-%     title = strrep(title, '-', ' - ');
-% 
-%     % Show all tracks
-%     plot_tracks(emg_Path, save_path, title, subjects(1).bmi)
-% 
-% end
-% fprintf("All tracks plots successfully created for RABA_ses1.\n")
-% 
-% % -- Build superimpose plots --
-% fprintf("\nCreating superimpose plots for RABA_ses1...\n")
-% plot_all_superimpose(data_dir, superimpose_dir)
-% fprintf("All superimpose plots successfully created for RABA_ses1.\n")
-% 
-% % -- Build CAI plots --
-% fprintf("\nComputing CAI for RABA_ses1...\n")
-% 
-% % Store last Rest file per group
-% last_rest_file = "";
-% 
-% for k = 1:numel(files)
-% 
-%     file_name = string(files(k).name);
-%     file_path = fullfile(data_dir, file_name);
-% 
-%     % Parse filename
-%     info = parse_filename(file_path);
-% 
-%     % Skip invalid files
-%     if isnan(info.group) || info.task_name == ""
-%         continue;
-%     end
-% 
-%     % Identify Rest file (per group!)
-%     if contains(info.task_name, "Rest")
-%         last_rest_file = file_path;
-%         fprintf("Rest file detected: %s\n", file_name);
-%         continue;
-%     end
-% 
-%     % Find matching movement key
-%     keys_list = movements.keys();
-%     matchedKey = "";
-% 
-%     for i = 1:length(keys_list)
-%         if startsWith(info.task_name, keys_list(i))
-%             matchedKey = keys_list(i);
-%             break;
-%         end
-%     end
-% 
-%     % If match found → compute CAI
-%     if matchedKey ~= ""
-% 
-%         % Check if Rest exists
-%         if last_rest_file == ""
-%             warning("No Rest file available yet. Skipping %s", file_name);
-%             continue;
-%         end
-% 
-%         file_path_rest = last_rest_file;
-% 
-%         Ag = movements(matchedKey).agoniste;
-%         Antag = movements(matchedKey).antagoniste;
-% 
-%         for j = 1:length(Ag)
-%             find_CAI( ...
-%                 last_rest_file, ...
-%                 file_path, ...
-%                 subjects(1).bmi, ...
-%                 "Ag", Ag(j), ...
-%                 "Antag", Antag(j), ...
-%                 "title", info.task_name, ...
-%                 "task", string(info.task), ...
-%                 "save_folder", CAI_dir, ...
-%                 "save_csv", CAI_results_path, ...
-%                 "subject", "RABA_ses1", ...
-%                 "device", info.device ...
-%             );
-%         end
-% 
-%     else
-%         warning("Task '%s' not recognized in dictionary.", info.task_name);
-%     end
-% 
-% end
-% fprintf("CAI computation completed for RABA_ses1.\n")
+% ------- SUBJECT 1 / SESSION 1 --------
 
+data_dir = "../data/GUEA_ses1";
+files = dir(fullfile(data_dir, '*.csv'));
+all_tracks_dir = "../figs/GUEA_ses1/all_tracks";
+superimpose_dir = "../figs/GUEA_ses1/superimpose";
+CAI_dir = "../figs/GUEA_ses1/CAI";
+
+% -- Build all tracks plot --
+fprintf("\nCreating all tracks plots for GUEA_ses1...\n")
+for k = 1:numel(files)
+
+    % Construct full input path
+    emg_Path = fullfile(files(k).folder, files(k).name);
+
+    % Get file name
+    file_name = string(files(k).name);
+    
+    % Get title
+    title = strrep(file_name, '.csv', '');
+    save_path = fullfile(all_tracks_dir, title);
+    title = strrep(title, '_', ' ');
+    title = strrep(title, '-', ' - ');
+     
+    % Show all tracks
+    if file_name == "Serie_3-Group_3-Task_31-JAMAR_Palmar_Grip-Device_(DynAReach).csv"
+        plot_tracks(emg_Path, save_path, title, subjects(1).bmi, ...
+        BicepTrack=1, TricepTrack=2, AntDeltTrack=5, PostDeltTrack=4)
+    else
+        plot_tracks(emg_Path, save_path, title, subjects(1).bmi)
+    end
+
+end
+fprintf("All tracks plots successfully created for GUEA_ses1.\n")
+
+% -- Build superimpose plots --
+fprintf("\nCreating superimpose plots for GUEA_ses1...\n")
+plot_all_superimpose(data_dir, superimpose_dir)
+fprintf("All superimpose plots successfully created for GUEA_ses1.\n")
+
+% -- Build CAI plots --
+fprintf("\nComputing CAI for GUEA_ses1...\n")
+
+% Store last Rest file per group
+last_rest_file = "";
+
+for k = 1:numel(files)
+
+    file_name = string(files(k).name);
+    file_path = fullfile(data_dir, file_name);
+
+    % Parse filename
+    info = parse_filename(file_path);
+
+    % Skip invalid files
+    if isnan(info.group) || info.task_name == ""
+        continue;
+    end
+
+    % Identify Rest file (per group!)
+    if contains(info.task_name, "Rest")
+        last_rest_file = file_path;
+        fprintf("Rest file detected: %s\n", file_name);
+        continue;
+    end
+
+    % Find matching movement key
+    keys_list = movements.keys();
+    matchedKey = "";
+
+    for i = 1:length(keys_list)
+        if startsWith(info.task_name, keys_list(i))
+            matchedKey = keys_list(i);
+            break;
+        end
+    end
+
+    % If match found → compute CAI
+    if matchedKey ~= ""
+
+        % Check if Rest exists
+        if last_rest_file == ""
+            warning("No Rest file available yet. Skipping %s", file_name);
+            continue;
+        end
+
+        file_path_rest = last_rest_file;
+
+        Ag = movements(matchedKey).agoniste;
+        Antag = movements(matchedKey).antagoniste;
+
+        for j = 1:length(Ag)
+            find_CAI( ...
+                last_rest_file, ...
+                file_path, ...
+                subjects(1).bmi, ...
+                "Ag", Ag(j), ...
+                "Antag", Antag(j), ...
+                "title", info.task_name, ...
+                "task", string(info.task), ...
+                "save_folder", CAI_dir, ...
+                "save_csv", CAI_results_path, ...
+                "subject","GUEA_ses1", ...
+                "device", info.device ...
+            );
+        end
+
+    else
+        warning("Task '%s' not recognized in dictionary.", info.task_name);
+    end
+
+end
+fprintf("CAI computation completed for GUEA_ses1.\n")
+
+
+% ------- SUBJECT 2 / SESSION 1 --------
+
+data_dir = "../data/RABA_ses1";
+files = dir(fullfile(data_dir, '*.csv'));
+all_tracks_dir = "../figs/RABA_ses1/all_tracks";
+superimpose_dir = "../figs/RABA_ses1/superimpose";
+CAI_dir = "../figs/RABA_ses1/CAI";
+
+% -- Build all tracks plot --
+fprintf("\nCreating all tracks plots for RABA_ses1...\n")
+for k = 1:numel(files)
+
+    % Construct full input path
+    emg_Path = fullfile(files(k).folder, files(k).name);
+
+    % Get file name
+    file_name = string(files(k).name);
+    
+    % Get title
+    title = strrep(file_name, '.csv', '');
+    save_path = fullfile(all_tracks_dir, title);
+    title = strrep(title, '_', ' ');
+    title = strrep(title, '-', ' - ');
+     
+    % Show all tracks
+    plot_tracks(emg_Path, save_path, title, subjects(1).bmi)
+
+end
+fprintf("All tracks plots successfully created for RABA_ses1.\n")
+
+% -- Build superimpose plots --
+fprintf("\nCreating superimpose plots for RABA_ses1...\n")
+plot_all_superimpose(data_dir, superimpose_dir)
+fprintf("All superimpose plots successfully created for RABA_ses1.\n")
+
+% -- Build CAI plots --
+fprintf("\nComputing CAI for RABA_ses1...\n")
+
+% Store last Rest file per group
+last_rest_file = "";
+
+for k = 1:numel(files)
+
+    file_name = string(files(k).name);
+    file_path = fullfile(data_dir, file_name);
+
+    % Parse filename
+    info = parse_filename(file_path);
+
+    % Skip invalid files
+    if isnan(info.group) || info.task_name == ""
+        continue;
+    end
+
+    % Identify Rest file (per group!)
+    if contains(info.task_name, "Rest")
+        last_rest_file = file_path;
+        fprintf("Rest file detected: %s\n", file_name);
+        continue;
+    end
+
+    % Find matching movement key
+    keys_list = movements.keys();
+    matchedKey = "";
+
+    for i = 1:length(keys_list)
+        if startsWith(info.task_name, keys_list(i))
+            matchedKey = keys_list(i);
+            break;
+        end
+    end
+
+    % If match found → compute CAI
+    if matchedKey ~= ""
+
+        % Check if Rest exists
+        if last_rest_file == ""
+            warning("No Rest file available yet. Skipping %s", file_name);
+            continue;
+        end
+
+        file_path_rest = last_rest_file;
+
+        Ag = movements(matchedKey).agoniste;
+        Antag = movements(matchedKey).antagoniste;
+
+        for j = 1:length(Ag)
+            find_CAI( ...
+                last_rest_file, ...
+                file_path, ...
+                subjects(1).bmi, ...
+                "Ag", Ag(j), ...
+                "Antag", Antag(j), ...
+                "title", info.task_name, ...
+                "task", string(info.task), ...
+                "save_folder", CAI_dir, ...
+                "save_csv", CAI_results_path, ...
+                "subject", "RABA_ses1", ...
+                "device", info.device ...
+            );
+        end
+
+    else
+        warning("Task '%s' not recognized in dictionary.", info.task_name);
+    end
+
+end
+fprintf("CAI computation completed for RABA_ses1.\n")
 
 % Create results file
+fprintf("\nCreating results CSV file...\n")
 initialize_results(results_path)
 
 % Add non-EMG-related results
 update_non_EMG_results(results_path)
 
 % Add CAI results
-input_path = "../res/resultsCAI.csv";
-output_path = "../res/results.csv";
-update_CAI_results(input_path, output_path);
+update_CAI_results(CAI_results_path, results_path);
+fprintf("Results have been written in %s\n", results_path)
